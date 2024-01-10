84aa4fe71cc3176ebf739cfdf9466ec68a8c5959
rafaeldavidmoreno <113324860+rafaeldavidmoreno@users.noreply.github.com>
2024-01-10 18:53:52 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,4 +17,24 @@ window.onscroll = () => {
     navbar.classList.remove('open');
 }
 
+/*  para el boton de leer mas leer menos */
+
+const parentContainer =  document.querySelector('.read-more-container');
+
+parentContainer.addEventListener('click', event=>{
+
+    const current = event.target;
+
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+
+    if(!isReadMoreBtn) return;
+
+    const currentText = event.target.parentNode.querySelector('.read-more-text');
+
+    currentText.classList.toggle('read-more-text--show');
+
+    current.textContent = current.textContent.includes('Leer más') ? "Leer menos..." : "Leer más...";
+
+})
+
     
